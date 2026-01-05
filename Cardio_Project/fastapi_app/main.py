6c5734e96b5496cace326fb1c4bfb51b74473537
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2026-01-05 17:12:19 +0530

    Update model file handling and remove local models
    Changed BASE_URL in main.py to point to Hugging Face for model downloads instead of GitHub. Uncommented fastapi_app/models/ in .gitignore and deleted local model and scaler files from the repository.

--- Cardio_Project/fastapi_app/main.py
+++ Cardio_Project/fastapi_app/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
 
 # Model files configuration
 MODEL_DIR = "models"
-BASE_URL = "https://github.com/mr-baraiya/Machine-Learning-Deep-Learning-hub/releases/download/v1.0-model"
+BASE_URL = "https://huggingface.co/mr-baraiya/cardio-disease-model/resolve/main"
 
 MODEL_FILES = {
     "random_forest_model.pkl": f"{BASE_URL}/random_forest_model.pkl",
